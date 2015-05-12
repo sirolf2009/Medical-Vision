ed1bbe8605fa91ffc494798fdb14e9cf5db6ba17
lach1994 <ali.lachhab1@gmail.com>
2015-05-12 12:42:00 +0200

    Android App
    App

---
+++ Medical-VisionApp/app/src/main/java/com/example/alilachhab/medical_visionapp/db/TaskContract.java
@@ -0,0 +1,16 @@
+package com.example.alilachhab.medical_visionapp.db;
+
+import android.provider.BaseColumns;
+
+public class TaskContract
+{
+    public static final String DB_NAME = "com.example.TodoList.db.tasks";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE = "tasks";
+
+    public class Columns {
+        public static final String TASK = "task";
+        public static final String _ID = BaseColumns._ID;
+    }
+}
+
